b4afaa9d21513037d53ad9cdaaa415844b3f270b
piyush97 <me@piyushmehta.com>
2018-05-19 14:06:49 +0530

    Api.js modded

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,22 @@
 const baseUrl = 'https://api.coinmarketcap.com/v2'
+
 export function getListings() {
   console.log('0. calling getListings...')
   return fetch(`${baseUrl}/listings/`)
+  .then( console.log('step 1, returning response.json()...') )
+  .then( response => response.json() )
+}
+
+export function getTicker() {
+  console.log('0. calling getTicker...')
+  return fetch(`${baseUrl}/ticker/`)
+  .then( console.log('step 1, returning response.json()...') )
+  .then( response => response.json() )
+}
+
+export function getGlobal() {
+  console.log('0. calling getGlobal...')
+  return fetch(`${baseUrl}/global/`)
   .then( console.log('step 1, returning response.json()...') )
   .then( response => response.json() )
 }
