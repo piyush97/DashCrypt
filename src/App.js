86e4820c33aeec88b3e98aea4c7ce0baeeb13850
piyush97 <me@piyushmehta.com>
2018-05-19 13:55:12 +0530

    App.js Made for Dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import DashboardContainer from './container/DashboardContainer'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <DashboardContainer />
+      </div>
+    );
+  }
+}
+export default App;
